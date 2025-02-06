de31a00a0c2eddadb813554d998f8e2cc436c345
kraja005 <41012260+kraja005@users.noreply.github.com>
2025-02-06 19:54:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "tfvmex-resources1"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "azurebackendstorage"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "backend"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "test.prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "test1.prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
